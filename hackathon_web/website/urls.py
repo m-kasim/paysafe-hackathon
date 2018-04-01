bc0a1ccc5ab18e75ea65ee6055fb3944974f0f0b
local <local@email.com>
2018-04-01 11:30:13 +0300

    Web: Django stuff WIP

--- hackathon_web/website/urls.py
+++ hackathon_web/website/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import url
 
 # NOTE: Import all [templates] manually here
-from .views import index
+from .views import index, detail
 ##from . import views
 
 urlpatterns = [
-    url(r'^$',                  index.index,                        name='index'),
+    url(r'^$',                  index.index,                    name='index'),
+    url(r'^detail',             detail.detail,                  name='detail'),
 ]
